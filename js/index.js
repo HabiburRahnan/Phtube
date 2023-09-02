9f861ba5eeaec5f18a504e2a237552b31e120ae5
unknown <habiburrahmanofficial157@gmail.com>
2023-09-02 14:20:08 +0600

    blog section and sec to convart hrs min

--- js/index.js
+++ js/index.js
@@ -6,13 +6,13 @@ const handleContainer = async () => {
   //   console.log(data.data);
   const tabContainer = document.getElementById("tab-container");
 
-  data.data.forEach((item) => {
+  data.data.forEach((category) => {
     const div = document.createElement("div");
     div.innerHTML = `
     <a onclick="handleCard(${
-      item.category_id
+      category.category_id
     })" class="tab text-2xl gap-4 text-center text-black rounded bg-gray-300 active:bg-red-600">${
-      item?.category ? item?.category : "no data found"
+      category?.category ? category?.category : "no data found"
     }</a> 
     `;
     tabContainer.appendChild(div);
@@ -20,32 +20,39 @@ const handleContainer = async () => {
   });
 };
 
-const handleCard = async (itemId) => {
+const handleCard = async (categoryId) => {
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
   const noData = document.getElementById("no-data");
   noData.innerHTML = "";
 
   const res = await fetch(`
-    https://openapi.programming-hero.com/api/videos/category/${itemId}
+    https://openapi.programming-hero.com/api/videos/category/${categoryId}
 
     `);
   const data = await res.json();
 
   if (data.status == true) {
-    data.data.forEach((item) => {
-      show(item?.others?.posted_date);
+    data.data.forEach((category) => {
       const div = document.createElement("div");
       //   console.log(item.authors);
-
+      // let seconds = data.data?.others?.posted_date;
+      // console.log(seconds);
+      // console.log(data.data?.others?.posted_date);
       div.innerHTML = `
-    <div class="card  bg-base-100 shadow-xl">
+    <div class="card  bg-base-100 shadow-xl ">
+      <div class="relative">
       <figure>
         <img class="h-52"
-          src="${item?.thumbnail}"
+          src="${category?.thumbnail}"
         />
       </figure>
-       
+      <div  class="absolute right-4 bottom-4 bg-black text-white"
+      >
+     ${category.others?.posted_date && show(category.others?.posted_date)}
+    
+      </div>
+      </div>
       <div class="card-body">
         <div class="card-footer flex justify-between mt-8">
             <div class="flex  justify-center text-center items-center gap-2">
@@ -53,21 +60,21 @@ const handleCard = async (itemId) => {
                 <div class="avatar online">
                   <div class="w-14 rounded-full flex">
                       <img
-                        src="${item?.authors[0]?.profile_picture}"/>
+                        src="${category?.authors[0]?.profile_picture}"/>
                   </div>
                 </div>
               </div>
                 <div>
                   <h2 class="card-title ">
-                  ${item?.title.slice(0, 20)}
+                  ${category?.title.slice(0, 20)}
                   </h2>
                 </div>
           </div>
         </div>
           <div class="flex justify-center gap-2">
-            <h2>${item?.authors[0]?.profile_name}</h2>
+            <h2>${category?.authors[0]?.profile_name}</h2>
             ${
-              item?.authors[0].verified
+              category?.authors[0].verified
                 ? `<img src="./image/fi_10629607.svg" alt="">`
                 : ""
             }
@@ -75,7 +82,7 @@ const handleCard = async (itemId) => {
           
           </div>
                 <div class="text-center">
-                  <h3>Total Views: ${item?.others?.views}</h3>
+                  <h3>Total Views: ${category?.others?.views}</h3>
                </div>
       </div>
     </div>
@@ -107,15 +114,16 @@ const fun = (data) => {
   }
 };
 // handleCard();
+
 const show = (seconds) => {
-  const timeShow = document.getElementById("time-show");
   parseInt.seconds;
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
   const remainingSeconds = seconds % 60;
-  const div = document.createElement("div");
-  div.innerHTML = `  ${hours}hrs ${minutes}min ${remainingSeconds}sec`;
+
+  return ` ${hours} hrs ${minutes} min ago`;
 };
-handleCard("1000")
+
+handleCard("1000");
 // console.log(show());
 handleContainer();
